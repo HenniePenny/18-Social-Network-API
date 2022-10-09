5fda452cf321e2d76f50e37f8a4af645c80f5c7f
HenniePenny <h.kressner@hotmail.de>
2022-10-09 17:56:50 +0200

    add delete user route

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,13 +2,16 @@ const {
   getUsers,
   createUser,
   getSingleUser,
+  deleteUser,
 } = require("../../controllers/userControllers");
 
 const router = require("express").Router();
 
 // /api/users
 router.route("/").get(getUsers).post(createUser);
+
+///api/users/:userId
 // router.get("/", getUsers)
-router.route("/:userId").get(getSingleUser);
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
 
 module.exports = router;
