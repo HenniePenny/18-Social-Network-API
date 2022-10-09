f0420ef6c1b024351dfe5c91eca4c8b1b94a87a4
HenniePenny <h.kressner@hotmail.de>
2022-10-09 10:57:05 +0200

    add User model

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const { Schema, Types } = require("mongoose");
+
+//import Thought and Reaction b/c using it as sub-documents
+const thoughtSchema = require("./Thought");
+const reactionSchema = require("./Reaction");
+
+//source: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+const emailValidation = (email) => {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [emailValidation, "Please fill in a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill in a valid email address",
+      ],
+    },
+    //array of _id values referencing the Thought model
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    //array _id values referencing the User model (self-reference)
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//create virtual called "friend count"
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
