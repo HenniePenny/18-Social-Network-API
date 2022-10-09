95851f36b42b1baf61bca546e1001607381ffa68
HenniePenny <h.kressner@hotmail.de>
2022-10-09 11:54:33 +0200

    add reactions schema

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionID: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    //!Use a getter method to format the timestamp on query
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = reactionSchema;
