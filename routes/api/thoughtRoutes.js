2cadeff6848899346d6029d503c82cb66a9a0d2a
HenniePenny <h.kressner@hotmail.de>
2022-10-11 20:06:34 +0200

    add post route for single thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,12 +1,13 @@
 const {
   getAllThoughts,
   getSingleThought,
+  createThought,
 } = require("../../controllers/thoughtController");
 
 const router = require("express").Router();
 
 // /api/thoughts
-router.route("/").get(getAllThoughts);
+router.route("/").get(getAllThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router.route("/:thoughtId").get(getSingleThought);
